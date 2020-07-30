11f7f33e55e05631a1f7eaca950bcf52cea1a6df
Leo Arnold <github@leoarnold.de>
2020-07-30 18:07:14 +0200

    Create task attachments directly from IOs
    We extend the `AttachmentUploading#attach` method to also accept IOs.
    
    This comes in handy when PDFs, CSV tables, ZIP files, etc.
    are generated in memory and shall be attached to an Asana task
    directly without writing to disk.
    
    Writing to disk not only makes the process slower and potentially
    clutters up the disk. Some users may have special data protection
    concerns where processing "data in flight" is acceptable
    but writing it to disk (i.e. "data at rest") is subject to
    higher compliance requirements.

--- spec/asana/resources/attachment_uploading_spec.rb
+++ spec/asana/resources/attachment_uploading_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'support/stub_api'
 require 'support/resources_helper'
 
@@ -26,17 +28,42 @@ RSpec.describe Asana::Resources::AttachmentUploading do
   end
 
   describe '#attach' do
-    it 'uploads an attachment to a unicorn' do
-      arg_matcher = ->(body) { body.is_a?(Faraday::CompositeReadIO) }
+    let(:arg_matcher) { ->(body) { body.is_a?(Faraday::CompositeReadIO) } }
+    let(:unicorn) { unicorn_class.new({ gid: '1' }, client: client) }
+
+    before do
       api.on(:post, '/unicorns/1/attachments', arg_matcher) do |response|
-        response.body = { data: { gid: "10" } }
+        response.body = { data: { gid: '10' } }
       end
-      unicorn = unicorn_class.new({ gid: "1" }, client: client)
-      attachment = unicorn.attach(filename: __FILE__,
-                                  mime: 'image/jpg',
-                                  name: 'file')
-      expect(attachment).to be_a(Asana::Resources::Attachment)
-      expect(attachment.gid).to eq("10")
+    end
+
+    context 'with a file from the file system' do
+      it 'uploads an attachment to a unicorn' do
+        attachment = unicorn.attach(filename: __FILE__,
+                                    mime: 'image/jpg')
+
+        expect(attachment).to be_a(Asana::Resources::Attachment)
+        expect(attachment.gid).to eq('10')
+      end
+    end
+
+    context 'with an IO' do
+      let(:io) do
+        ::StringIO.new(<<~CSV)
+          Employee;Salary
+          "Bill Lumbergh";70000
+          "Peter Gibbons";40000
+        CSV
+      end
+
+      it 'uploads an attachment to a unicorn' do
+        attachment = unicorn.attach(io: io,
+                                    mime: 'text/csv',
+                                    filename: 'salaries.csv')
+
+        expect(attachment).to be_a(Asana::Resources::Attachment)
+        expect(attachment.gid).to eq('10')
+      end
     end
   end
 end
