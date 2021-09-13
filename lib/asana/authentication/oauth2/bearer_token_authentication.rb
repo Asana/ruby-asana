058cbca8458abb900a1e8278ee24e5d97b605aa7
Ricky Chilcott <ricky@rakefire.io>
2021-09-13 10:51:06 -0400

    Remove Faraday connection authorization warnings

--- lib/asana/authentication/oauth2/bearer_token_authentication.rb
+++ lib/asana/authentication/oauth2/bearer_token_authentication.rb
@@ -24,7 +24,7 @@ module Asana
         #
         # Returns nothing.
         def configure(connection)
-          connection.authorization :Bearer, @token
+          connection.request :authorization, :Bearer, @token
           connection.request :oauth2, token_type: 'bearer'
         end
       end
